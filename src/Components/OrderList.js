0179225e3daf8a662fd5ae8809fa4a9a8b16b7e2
Nabil Khan <nabilkhan228@gmail.com>
2021-05-20 20:37:20 -0700

    search works now

--- src/Components/OrderList.js
+++ src/Components/OrderList.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import '../css/orderlist.css'
 
-function OderList(){
+function OderList({data}){
+  
   return(
-<div className="register">
-  <div className="left-side">
-    <div className="order-window">
-      <table>
-        <tbody>
-          <tr>
-            <td>Number</td>
-            <td>Item</td>
-            <td>SKUnumber</td>
-            <td>Description</td>
-            <td>Price</td>
-            <td>In Stock</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-   </div>
-</div>);
+  <div className="register">
+    {console.log(data)}
+    <li>Product Name: {data.fields.ProductName}</li>
+    <li>SKU number: {data.fields.SKU}</li>
+    <li>Stock: {data.fields.InStock}</li>
+    <li>Color: {data.fields.Color}</li>
+    <li>Price: {data.fields.Price}</li>
+    <li>Description: {data.fields.Description}</li>
+  </div>
+  );
 }
 
 export default OderList;
